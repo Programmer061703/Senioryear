f986534d1bbee14fa35f66f10ba55ca20e45b85c
coder061703 <blaker1703@gmail.com>
2024-08-29 13:00:52 -0500

    Updated: ceaser.cpp to use command line args

--- first_semester/Crypto/HW1/ceaser.cpp
+++ first_semester/Crypto/HW1/ceaser.cpp
@@ -48,42 +48,33 @@ string ceaser_decrypt(string text, int shift){
 }
 
 
-int main() {
-    string input = "";
-    int shift;
-    char mode_select;
+int main(int argc, char* argv[]) {
+    // Ensure the correct number of arguments
+    if (argc != 4) {
+        cerr << "Usage: " << argv[0] << " <1 or 2> <input_text(must have quotes around text)> <shift_value>\n" 
+             << "\t\t1: Encrypt 2: Decrypt" << endl;
+        return 1;
+    }
 
+    // Get the mode from the first argument
+    char mode_select = argv[1][0];  // First character of argv[1]
 
-    cout << "Select Mode (1: Encrypt, 2: Decrypt): ";
-    cin >> mode_select;
-
-
-    cin.ignore();
+    // Get the input string and shift value from the command line arguments
+    string input = argv[2];
+    int shift = stoi(argv[3]);
 
     switch (mode_select) {
         case '1': // Encrypt mode
-            cout << "Input String: ";
-            getline(cin, input);
-
-            cout << "Input Shift value: ";
-            cin >> shift;
-
             cout << "\nEncrypted String: " << ceaser_encrypt(input, shift) << endl;
             break;
 
         case '2': // Decrypt mode
-            cout << "Input String: ";
-            getline(cin, input);
-
-            cout << "Input Shift value: ";
-            cin >> shift;
-
             cout << "\nDecrypted String: " << ceaser_decrypt(input, shift) << endl;
             break;
 
         default:
-            cout << "Bad Input, Try again" << endl;
-            break;
+            cerr << "Invalid mode selected. Use '1' for Encrypt or '2' for Decrypt." << endl;
+            return 1;
     }
 
     return 0;
